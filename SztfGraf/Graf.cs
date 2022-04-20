7691805e648ea69a2b6d59cd91db9ba49f36bd70
TorokDan <dani.torok8@gmail.com>
2022-04-20 11:33:07 +0200

    Szélességi bejárás

--- SztfGraf/Graf.cs
+++ SztfGraf/Graf.cs
@@ -4,6 +4,7 @@ namespace SztfGraf
 {
     public abstract class Graf<T>
     {
+        public delegate void BejaroHandler(T tartalom);
         protected class El
         {
             public T hova;
@@ -11,6 +12,33 @@ namespace SztfGraf
         }
         public abstract void UjCsucs(T tartalom);
         public abstract void UjEl(T honnan, T hova, double suly = 1, bool iranyitott = false);
+
+        protected abstract List<El> Szomszedok(T csucs);
+
+        public void SzelessegiBejaras(T honnan, BejaroHandler _metodus)
+        {
+            BejaroHandler metodus = _metodus;
+            Queue<T> S = new Queue<T>();
+            List<T> F = new List<T>();
+            
+            S.Enqueue(honnan);
+            F.Add(honnan);
+            while (S.Count != 0)
+            {
+                T K = S.Dequeue();
+                metodus?.Invoke(K);
+
+                foreach (El x in Szomszedok(K))
+                {
+                    if (!F.Contains(x.hova))
+                    {
+                        S.Enqueue(x.hova);
+                        F.Add(x.hova);
+                    }
+                }
+            }
+
+        }
     }
 
     class GrafSzomszedsagiLista<T> : Graf<T>
@@ -49,5 +77,7 @@ namespace SztfGraf
                 });
             }
         }
+
+        protected override List<El> Szomszedok(T csucs) => _szomszedok[_tartalmak.IndexOf(csucs)];
     }
 }
--- SztfGraf/Program.cs
+++ SztfGraf/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace SztfGraf
 {
     class Program
@@ -10,10 +12,38 @@ namespace SztfGraf
             graf.UjCsucs("Debrecen");
             graf.UjCsucs("Budapest");
             graf.UjCsucs("Pécs");
+            graf.UjCsucs("Győr");
+            graf.UjCsucs("Siófok");
+            graf.UjCsucs("Sopron");
+            graf.UjCsucs("Budapest");
+            graf.UjCsucs("Eger");
+            graf.UjCsucs("Szolnok");
+            graf.UjCsucs("Miskolc");
+            graf.UjCsucs("Szeged");
             
-            graf.UjEl("Budapest", "Pécs", 180 );
-            graf.UjEl("Budapest", "Debrecen", 200 );
-            ;
+            graf.UjEl("Budapest", "Eger", 140 );
+            graf.UjEl("Eger", "Miskolc", 60 );
+            graf.UjEl("Miskolc", "Debrecen", 102 );
+            graf.UjEl("Debrecen", "Szeged", 228 );
+            graf.UjEl("Szeged", "Pécs", 189 );
+            graf.UjEl("Pécs", "Sopron", 277 );
+            graf.UjEl("Győr", "Sopron", 93 );
+            graf.UjEl("Győr", "Budapest", 121 );
+            graf.UjEl("Budapest", "Szolnok", 111 );
+            graf.UjEl("Eger", "Szolnok", 108 );
+            graf.UjEl("Miskolc", "Szolnok", 153 );
+            graf.UjEl("Debrecen", "Szolnok", 131 );
+            graf.UjEl("Szeged", "Szolnok", 119 );
+            graf.UjEl("Budapest", "Szeged", 175 );
+            graf.UjEl("Budapest", "Pécs", 209 );
+            graf.UjEl("Budapest", "Siófok", 104 );
+            graf.UjEl("Győr", "Siófok", 135 );
+            graf.UjEl("Siófok", "Sopron", 191 );
+            graf.UjEl("Pécs", "Siófok", 115 );
+            graf.UjEl("Siófok", "Szolnok", 232 );
+            
+            
+            graf.SzelessegiBejaras("Sopron", Console.WriteLine );
         }
     }
 }
